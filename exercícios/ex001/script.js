cf2105760c23901444fc8db917799c29eeaeed9a
Phaionner <phaionnerfranciolli@gmail.com>
2023-10-05 18:23:23 -0300

    ex 002

--- exercícios/ex001/script.js
+++ exercícios/ex001/script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagen')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'imagemanha.png'
+        document.body.style.background = '#ffcb6e'
+    } else if (hora >= 12 && hora <= 18){
+        //BOA TARDE
+        img.src = 'imagemtarde.png'
+        document.body.style.background = '#b98b69'
+    } else {
+        //BOA NOITE
+        img.src = 'imagemnoite.png'
+        document.body.style.background = '#171e2e'
+    }
+}

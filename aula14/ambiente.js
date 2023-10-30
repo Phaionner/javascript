bc801c7b28f5f66bbfa05d9f977fc6895e1c21f5
Phaionner <phaionnerfranciolli@gmail.com>
2023-10-30 16:53:15 -0300

    Update ambiente.js

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -8,7 +8,7 @@ while(c <= 5) {
 
 //-----------------------------//
 console.log('Vai começar...')
-for(var c = 1;c <=5;c++) {
+for(var c = 1;c <= 10;c++) {
     console.log(c)
 }
 console.log('FIM!')
